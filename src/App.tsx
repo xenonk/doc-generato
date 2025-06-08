e179c99dce8ccee8453b21f6fb3112c7bc26c32c
Xenon <xenon.k90@gmail.com>
2025-06-08 18:28:43 +0300

    deleting tsx and adding new

--- src/App.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Dashboard from './pages/Dashboard';
-
-function App() {
-  return <Dashboard />;
-}
-
-export default App; 
--- src/index.tsx
+++ src/index.tsx
@@ -15,7 +15,10 @@ const queryClient = new QueryClient({
   },
 });
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const rootElement = document.getElementById('root');
+if (!rootElement) throw new Error('Failed to find the root element');
+
+const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
