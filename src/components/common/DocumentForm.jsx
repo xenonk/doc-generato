d18aa8a1ca489c7c580ec39f727c02b468eb1e3d
Xenon <xenon.k90@gmail.com>
2025-06-10 23:00:16 +0300

    removed redutant save button near items table

--- src/components/common/DocumentForm.jsx
+++ src/components/common/DocumentForm.jsx
@@ -78,18 +78,6 @@ const DocumentForm = ({
           </div>
         );
       })}
-      {/* Save button example */}
-      {onSave && (
-        <div className="flex justify-end">
-          <button
-            onClick={() => onSave('draft')}
-            className="bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700"
-            disabled={isSaving}
-          >
-            {isSaving ? 'Saving...' : 'Save'}
-          </button>
-        </div>
-      )}
     </div>
   );
 };
